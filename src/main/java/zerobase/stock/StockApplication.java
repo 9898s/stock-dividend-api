97baefa4cbdec535def55a88ad5780f5d53e418d
9898s <sueh9898@icloud.com>
2023-07-24 18:17:44 +0900

    자동 완성

--- src/main/java/zerobase/stock/StockApplication.java
+++ src/main/java/zerobase/stock/StockApplication.java
@@ -2,8 +2,6 @@ package zerobase.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zerobase.stock.scraper.Scraper;
-import zerobase.stock.scraper.YahooFinanceScraper;
 
 @SpringBootApplication
 public class StockApplication {
--- src/main/java/zerobase/stock/web/CompanyController.java
+++ src/main/java/zerobase/stock/web/CompanyController.java
@@ -18,7 +18,8 @@ public class CompanyController {
 
     @GetMapping("/autocomplete")
     public ResponseEntity<?> autocomplete(@RequestParam String keyword) {
-        return null;
+        var result = this.companyService.getCompanyNamesByKeyword(keyword);
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping
@@ -41,6 +42,7 @@ public class CompanyController {
         }
 
         Company company = this.companyService.save(ticker);
+        this.companyService.addAutoCompleteKeyword(company.getName());
         return ResponseEntity.ok(company);
     }
 
