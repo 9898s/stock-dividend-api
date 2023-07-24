886ed6d2379251e135f25415d0f93dec0c0309ca
9898s <sueh9898@icloud.com>
2023-07-24 14:00:53 +0900

    배당금 저장

---
+++ src/main/java/zerobase/stock/model/Dividend.java
@@ -0,0 +1,13 @@
+package zerobase.stock.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class Dividend {
+    private LocalDateTime date;
+    private String dividend;
+}
